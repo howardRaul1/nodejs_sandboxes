9cce3490a6d42d7ca2e1868493427caebe017251
howardRaul1 <61098455+howardRaul1@users.noreply.github.com>
2023-04-30 19:41:00 -0400

    q2 codes work locally

--- Stanford_cs142/project2/cs142-template-processor.js
+++ Stanford_cs142/project2/cs142-template-processor.js
@@ -1,7 +1,7 @@
 // noinspection ES6ConvertVarToLetConst
 
 "use strict";
-var assert = require('assert')
+//var assert = require('assert')
 
 class Cs142TemplateProcessor { // class: start ///////////////////////////////////
   constructor(origStr) { this.origStr = origStr;} // instance public variable
@@ -20,19 +20,3 @@ class Cs142TemplateProcessor { // class: start /////////////////////////////////
   }
 
 } // class: end ////////////////////////////////////
-
-
-var template = "My favorite month is {{month}} but not the day {{day}} or the year {{year}}";
-var dateTemplate = new Cs142TemplateProcessor(template);
-
-var dictionary = {month: "July", day: "1", year: "2016"};
-var str = dateTemplate.fillIn(dictionary);
-
-assert(str === "My favorite month is July but not the day 1 or the year 2016");
-
-// Case: property doesn't exist in dictionary
-var dictionary2 = {day: "1", year: "2016"};
-var str = dateTemplate.fillIn(dictionary2);
-
-assert(str === "My favorite month is  but not the day 1 or the year 2016");
-
