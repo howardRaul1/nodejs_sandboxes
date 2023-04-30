16a1092c433efeba41ffa87a88670f2ba82837be
howardRaul1 <61098455+howardRaul1@users.noreply.github.com>
2023-04-29 20:58:26 -0400

    basic fn of q1 (in cs142 proj 1) worked up to line 26

--- Stanford_cs142/project2/q1/cs142-make-multi-filter.js
+++ Stanford_cs142/project2/q1/cs142-make-multi-filter.js
@@ -4,19 +4,40 @@ function cs142MakeMultiFilter (originalArray) {
 	return function arrayFilterer(filterCriteria , callback ) {
 		if (typeof filterCriteria !== 'function') return currentArray;
 		for (const k in currentArray) if ( ! filterCriteria(currentArray[k]) ) currentArray.splice(k,1);
-		callback.call(originalArray, currentArray);
+
+		if (typeof callback === 'function') {
+			callback.call(originalArray, currentArray);
+
+		}
+		return arrayFilterer;
 	};
 }
+// ***********************************************************************
 
 
 var arrayFilterer1 = cs142MakeMultiFilter([1, 2, 3]);
 
-// Call arrayFilterer1 (with a callback function) to filter out all the numbers
-// not equal to 2.
-arrayFilterer1(function (elem) {
-	return elem !== 2; // check if element is not equal to 2
-}, function (currentArray) {
-	// 'this' within the callback function should refer to originalArray which is [1, 2, 3]
-	console.log(this); // prints [1, 2, 3]
-	console.log(currentArray); // prints [1, 3]
-});
+// Call arrayFilterer1 (with a callback function) to filter out all the numbers not equal to 2.
+arrayFilterer1(
+
+	function (elem) {
+		return elem !== 2; // check if element is not equal to 2
+	}
+
+	,
+
+	function (currentArray) {// 'this' within the callback function should refer to originalArray which is [1, 2, 3]
+		console.log(this); // prints [1, 2, 3]
+		console.log(currentArray); // prints [1, 3]
+	}
+
+);
+
+// Call arrayFilterer1 (without a callback function) to filter out all the elements not equal to 3.
+arrayFilterer1(function (elem) {return elem !== 3;});
+
+// Calling arrayFilterer1 with no filterCriteria should return the currentArray.
+var currentArray = arrayFilterer1();
+console.log("currentArray", currentArray); // prints [1] since we filtered out 2 and 3
+
+
