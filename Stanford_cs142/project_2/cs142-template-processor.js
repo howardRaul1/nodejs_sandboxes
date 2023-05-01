0b46105d81ae11bd011ab0b46599f0f790f3dc84
howardRaul1 <61098455+howardRaul1@users.noreply.github.com>
2023-04-30 20:19:29 -0400

    passed eslint

--- Stanford_cs142/project_2/cs142-template-processor.js
+++ Stanford_cs142/project_2/cs142-template-processor.js
@@ -1,13 +1,13 @@
 "use strict";
 
- function Cs142TemplateProcessor(origStr) { // function-class: start ///////////////////////////////////
+ function Cs142TemplateProcessor(origStr) { // function-class: start ////////////////
   this.origStr = origStr; // instance public variable
 
   this.fillIn = function(dict) {
     let currStr = this.origStr;
 
     for (const [k, v] of Object.entries(dict)) {
-      let fullKey = '{{' + k + '}}';
+      const fullKey = '{{' + k + '}}';
       if (currStr.includes(fullKey)) currStr = currStr.replace(fullKey, "" + v);
     }
     if (currStr.includes('{{')) {
