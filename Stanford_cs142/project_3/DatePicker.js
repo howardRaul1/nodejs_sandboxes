57e72528381ce2a271eeebe477e946a689d7cbf6
howardRaul1 <61098455+howardRaul1@users.noreply.github.com>
2023-05-01 19:41:12 -0400

    was able to print callback-bound event-triggering DOM element id, as well as click-action target (child) element id

--- Stanford_cs142/project_3/DatePicker.js
+++ Stanford_cs142/project_3/DatePicker.js
@@ -1,7 +1,19 @@
-var datePicker = new DatePicker("div1", function (id, fixedDate) {
-  console.log("DatePicker with id", id,
-    "selected date:", fixedDate.month + "/" + fixedDate.day + "/" + fixedDate.year);
-});
-datePicker.render(new Date("July 4, 1776"));
+// var datePicker = new DatePicker("div1", function (id, fixedDate) {
+//   console.log("DatePicker with id", id,
+//     "selected date:", fixedDate.month + "/" + fixedDate.day + "/" + fixedDate.year);
+// });
+// datePicker.render(new Date("July 4, 1776"));
+
+
+function clickCb(event) {
+  // alert('hi');
+  console.log('target id is: ' + event.target.id);
+  console.log("callback-bound DOM element id = " + this.id);
+
+}
+
+document.getElementById('dp1_1').addEventListener("click", clickCb);
+
+console.log("Loaded DatePicker.js -----");
 
 
